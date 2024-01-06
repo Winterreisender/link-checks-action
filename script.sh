a56cb2332d6f783e631e0802de315f76b4e03b9b
winteru <winterreisender@foxmail.com>
2024-01-06 16:58:11 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,36 +1,12 @@
 #!/bin/bash
 
-# Change this value to match the name of your Kotlin file
 FILE="$1"
 
-# Split the file names into an array
-IFS=',' read -ra FILE_ARRAY <<< "$FILE"
-
-# Use grep to find all the URLs in each file
-URLS=""
-for filename in "${FILE_ARRAY[@]}"
-do
-    urls=$(grep -o 'https\?://[a-zA-Z0-9./?=_&-]\+' $filename)
-    URLS="$URLS $urls"
+IFS=$'\n';
+for i in `cat $FILE | tr -d '\n' | grep -oP '(<a href="http[s]?://.*?>.*?</a>)'`; do
+    echo $i | grep -oP '(?<=>).*?(?=<)' | xargs echo -n
+    echo -e -n "\t"
+    echo $i | grep -oP '(?<=<a href=")(http[s]?://[^"]*)' | xargs echo -n
+    echo -e -n "\t"
+    echo $i | grep -oP '(?<=<a href=")(http[s]?://[^"]*)' | xargs -L 1 curl -I -m 5 -s -w "%{http_code}\n" -o /dev/null
 done
-
-# Check the status code of each URL with curl
-i=0
-failed_urls=()
-for url in $URLS
-do
-    status_code=$(curl --silent --head --output /dev/null --write-out '%{http_code}' $url)
-    if [ $status_code -ne 200 ]; then
-      echo "$i: $url - Failed (HTTP $status_code)"
-      failed_urls+=("$url")
-    fi
-    ((i++))
-done
-
-
-# If there were failed URLs, exit with an error
-if [ ${#failed_urls[@]} -gt 0 ]; then
-    echo "The following URLs failed:"
-    printf '%s\n' "${failed_urls[@]}"
-    exit 1
-fi
